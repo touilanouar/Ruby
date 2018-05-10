3967a2b1b638c84a2134d4d55fcb39f7ee4aa6fc
1611681@stud.hs-mannheim.de <230687-Fbana>
2018-05-10 22:32:12 +0200

    transform test

--- cracker.rb
+++ cracker.rb
@@ -3,9 +3,7 @@ require 'digest'
 #testen mittels kj-bible.txt
 def test_pass(value,salt)
   File.foreach('C:\Users\anoua\Documents\GitHub\Ruby\kj-bible.txt') do |word|
-
     kelma =salt+ (Digest::MD5.hexdigest (word.chomp))
-    puts kelma
     if kelma == value
       return puts '[+] Found Password: ' + word
     end
@@ -17,12 +15,22 @@ end
 def trans_txt()
   datastring = ""
   File.foreach('C:\Users\anoua\Documents\GitHub\Ruby\bullshit.txt') do |word|
-    datastring = datastring + word.gsub("e","3")
+    datastring = datastring + word.gsub("a","3")
     end
-    data = File.new('C:\Users\anoua\Documents\GitHub\Ruby\test.txt', "w")
+    data = File.new('C:\Users\anoua\Documents\GitHub\Ruby\transform.txt', "w")
     data.write(datastring)
     data.close
     puts"Datei transformed..."
+end
+# test mittels transform.txt
+def test_pass_transform(value,salt)
+  File.foreach('C:\Users\anoua\Documents\GitHub\Ruby\transform.txt') do |word|
+    kelma =salt+ (Digest::MD5.hexdigest (word.chomp))
+    if kelma == value
+      return puts '[+] Found Password: ' + word
+    end
+    end
+  puts '[-] Password Not Found'#end
 end
 
 
@@ -33,3 +41,5 @@ end
     test_pass(eingabe,salt)
     puts"'********************bullshit.txt'...transformation to 'test.txt'*******"
     trans_txt()
+    puts"**************Testing words from 'transform.txt'...********************"
+    test_pass_transform(eingabe,salt)
